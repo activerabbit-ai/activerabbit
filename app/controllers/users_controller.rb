9209cb14b554df70d288e34030a1a89c63eda6ee
Alex Shapalov <shapalov@gmail.com>
2026-01-02 18:13:29 -0800

    Enhance user creation form by adding role parameter and improving error display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,7 +62,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :role, :password, :password_confirmation)
   end
 
   def current_account
