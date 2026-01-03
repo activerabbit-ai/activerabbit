575b11b3907163bb0a458010f9ac6a5ccd7b2cce
Alex Shapalov <shapalov@gmail.com>
2026-01-03 15:07:31 -0800

    Enhance weekly report email delivery and subject formatting

--- app/jobs/weekly_report_job.rb
+++ app/jobs/weekly_report_job.rb
@@ -6,11 +6,11 @@ class WeeklyReportJob
       ActsAsTenant.with_tenant(account) do
         report = WeeklyReportBuilder.new(account).build
 
-        account.users.find_each do |user|
+        account.users.find_each.with_index do |user, index|
           WeeklyReportMailer
-            .with(user: user, report: report)
+            .with(user: user, account: account, report: report)
             .weekly_report
-            .deliver_later
+            .deliver_later(wait: index.seconds)
         end
       end
     end
--- app/mailers/weekly_report_mailer.rb
+++ app/mailers/weekly_report_mailer.rb
@@ -1,14 +1,18 @@
 class WeeklyReportMailer < ApplicationMailer
   def weekly_report
     @user = params[:user]
+    @account = params[:account]
     @report = params[:report]
     @dashboard_url = dashboard_url(
       host: ENV.fetch("APP_HOST", "localhost:3000")
     )
 
+    period_start = @report[:period].first.strftime("%B %d, %Y")
+    period_end = @report[:period].last.strftime("%B %d, %Y")
+
     mail(
       to: @user.email,
-      subject: "Your weekly system report"
+      subject: "Weekly Report for #{@account.name}: #{period_start} - #{period_end}"
     )
   end
 end
